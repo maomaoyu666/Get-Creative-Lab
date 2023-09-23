722884f6df23cd199390d1e430e38cf3a16eb1e1
Jing Liu <116334673+maomaoyu666@users.noreply.github.com>
2023-09-23 14:05:01 -0700

    Update SimpleProgram.java

--- SimpleProgram.java
+++ SimpleProgram.java
@@ -33,6 +33,88 @@ public class SimpleProgram {
         
         // Close the scanner
         scanner.close();
+
+        // Perform all Java data types and operations(Arithmetic Operators, ) I learned
+        // Data types:
+        // 1. boolean
+        // 2. byte
+        // 3. short
+        // 4. int
+        // 5. long
+        // 6. double
+        // 7. float
+        // 8. char
+        // 9. String
+
+        // 1. Boolean
+        boolean a = true;
+        boolean b = false;
+        System.out.println(a == b);
+        System.out.println(a && b);
+        System.out.println(a || b);
+        System.out.println(!a);
+
+        // 2. Byte
+        byte c = 1;
+        System.out.println(c++);
+        System.out.println(++c);
+
+        // 3. short
+        short d = 128;
+        System.out.println(d--);
+        System.out.println(--d);
+
+        // 4. int
+        int e = 10000;
+        int f = 20000;
+        System.out.println(e + f);
+        System.out.println(e - f);
+        System.out.println(e * f);
+        System.out.println(e / f);
+        System.out.println(e % f);
+        e += 1;
+        f -= 1;
+        System.out.println(e);
+        System.out.println(f);
+
+        // 5. long
+        long g = 2 ^ 33 + 1;
+        long h = - 2 ^ 33;
+        g /= h;
+        System.out.println(g);
+        g *= h;
+        System.out.println(g);
+        g %= h;
+        System.out.println(g);
+        System.out.println(g > h);
+        System.out.println(g >= h);
+        System.out.println(g < h);
+        System.out.println(g <= h);
+
+        // 6. double
+        double i = 228.00;
+        double j = 374.00;
+        System.out.println(i / j);
+
+        // 7. float
+        float m = 3.4f;
+        float n = 44.5f;
+        System.out.println(m - n);
+
+        // 8. char
+        char p = 'a';
+        char q = 'b';
+        System.out.println(p == q);
+        System.out.println(p != q);
+
+        // 9. string
+        String y = "Ilovetechbridge";
+        String z = "Ithinkso";
+        System.out.println(y + z);
+        System.out.println(y == z);
+        System.out.println(y != z);        
+
+
     }
 }
 
